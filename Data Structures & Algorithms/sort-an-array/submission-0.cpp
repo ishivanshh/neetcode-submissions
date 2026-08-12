d43789f4cfaae3e4c8c2c79a0fd6ebe2e7b0ec30
Shivansh Saxena <i.shivanshhh@gmail.com>
2026-08-12 09:24:44 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        quickSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+    void quickSort(vector<int>& nums, int low, int high) {
+        if(low < high) {
+            int pindex = partition(nums, low, high);
+            quickSort(nums, low, pindex - 1);
+            quickSort(nums, pindex + 1, high);
+        }
+    }
+    int partition(vector<int>& nums, int low, int high) {
+        int pivot = nums[low];
+        int i = low;
+        int j = high;
+        while(i < j) {
+            while(i <= high && nums[i] <= pivot) {
+                i++;
+            }
+            while(j >= low && nums[j] > pivot) {
+                j--;
+            }
+            if(i < j) {
+                swap(nums[i], nums[j]);
+            }
+        }
+        swap(nums[low], nums[j]);
+        return j;
+    }
+};
