e71ae3493adb22c8005c88ea62f8ebf4ac7ac78b
Shivansh Saxena <i.shivanshhh@gmail.com>
2026-08-25 15:44:27 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int>st;
+         for (auto it : s) {
+            if (it == '(' || it == '{' || it == '[')
+                st.push(it);  // Push opening brackets to stack
+            else {
+                if (st.empty()) return false;  // No matching opening bracket
+                char ch = st.top();
+                st.pop();
+
+                // Check for matching pair
+                if ((it == ')' && ch == '(') ||
+                    (it == ']' && ch == '[') ||
+                    (it == '}' && ch == '{'))
+                    continue;
+                else
+                    return false;
+            }
+        }
+        return st.empty();  // True if all brackets matched
+    }
+};
