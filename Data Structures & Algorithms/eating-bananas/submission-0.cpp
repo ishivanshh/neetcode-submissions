e41251a9a733996bbaa63f42a1a5d163dd0fdd4e
Shivansh Saxena <i.shivanshhh@gmail.com>
2026-08-31 14:40:14 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // maximun element in the piles
+    int findMax(vector<int> & piles){
+        int maxi = INT_MIN;
+        for(int i = 0; i < piles.size(); i++){
+            maxi = max(piles[i], maxi);
+        }
+        return maxi;
+    }
+    // calculation of total hrs 
+    long long calculateTotalHours(vector<int>& piles ,int hourly){
+        long long totalH = 0;
+        for(int i = 0; i < piles.size(); i++){
+            totalH += (piles[i] + hourly - 1) / hourly;
+        }
+        return totalH;
+    }
+    
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = findMax(piles);
+
+        while (low <= high){
+            int mid = low + (high - low) / 2;
+
+            long long totalH = calculateTotalHours(piles, mid);
+
+            if(totalH <= h){
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
